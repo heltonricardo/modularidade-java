b99ca433a902d9bc38cda4f735e34b44ddec1156
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-17 11:04:49 -0300

    Update module-info.java

--- app-financeiro/src/module-info.java
+++ app-financeiro/src/module-info.java
@@ -1,2 +1,9 @@
 module app.financeiro {
+	
+	/*
+	 * Listagem dos pacotes exportados por outros projetos que serão usados
+	 * nesse projeto. Por padrão o pacote java.base já é importado.
+	 */
+	
+	requires app.calculo;
 }
