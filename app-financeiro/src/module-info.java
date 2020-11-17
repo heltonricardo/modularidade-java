824c538a2ed9d02b128a5faa244b03660c927ad5
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-17 14:48:07 -0300

    Update module-info.java

--- app-financeiro/src/module-info.java
+++ app-financeiro/src/module-info.java
@@ -2,8 +2,14 @@ module app.financeiro {
 	
 	/*
 	 * Listagem dos pacotes exportados por outros projetos que serão usados
-	 * nesse projeto. Por padrão o pacote java.base já é importado.
+	 * nesse projeto. Por padrão o pacote java.base já é importado. Para
+	 * usar o requires é necessário adicionar o outro projeto em Build Path:
+	 * Botão direito no projeto > Build Path > Configure Build Path... > Aba
+	 * Projects > Selecione modulepath > Add > Selecione o projeto para
+	 * importar > Ok > Apply and Close. Em seguida já será possível usar o
+	 * requires aqui dos pacotes exportados pelo projeto adicionado.
 	 */
 	
 	requires app.calculo;
+	requires app.login;
 }
