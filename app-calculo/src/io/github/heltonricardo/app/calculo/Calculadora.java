ea1d99ecee2ffddbda01598ad6a26fb187efb0d2
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-17 14:47:59 -0300

    Update Calculadora.java

--- app-calculo/src/io/github/heltonricardo/app/calculo/Calculadora.java
+++ app-calculo/src/io/github/heltonricardo/app/calculo/Calculadora.java
@@ -1,13 +1,18 @@
 package io.github.heltonricardo.app.calculo;
 
 import io.github.heltonricardo.app.calculo.interno.OperacoesAritmeticas;
+import io.github.heltonricardo.app.login.Logger;
 
 public class Calculadora {
 
 	private OperacoesAritmeticas opArit = new OperacoesAritmeticas();
 
-	// Método Delegate (delega a funcionalidade para um método já implementado)
 	public double soma(double... nums) {
+		Logger.info("Somando");
 		return opArit.soma(nums);
+	}
+	
+	public Class<Logger> getLoggerClass() {
+		return Logger.class;
 	}
 }
