78262f5d8784a3d8d5a40bfb8d8e2a2987ca7e4d
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-17 11:05:07 -0300

    Create Teste.java

---
+++ app-financeiro/src/io/github/heltonricardo/app/financeiro/Teste.java
@@ -0,0 +1,13 @@
+package io.github.heltonricardo.app.financeiro;
+
+import io.github.heltonricardo.app.calculo.Calculadora;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Calculadora calc = new Calculadora();
+		
+		System.out.println(calc.soma(1.1, 2.2, 3.3, 4.4, 5.5));
+	}
+}
