d57f3b453fe5db6e4455d6a231e35504f9ad3f16
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-17 14:48:01 -0300

    Update module-info.java

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -4,7 +4,12 @@ module app.calculo {
 	 * Lista de pacotes que serão exportados (podem ser usados fora do projeto)
 	 * Os pacotes internos não são exportados por padrão, todos os pacotes
 	 * devem ser descritos aqui para que sejam visíveis para outros projetos.
+	 * 
+	 * Usamos o transitive quando queremos que quem importar esse projeto
+	 * também importe o transitivo, pois há uma dependência de códigos que
+	 * estão no projeto que esse projeto importou.
 	 */
 	
+	requires transitive app.login;
 	exports io.github.heltonricardo.app.calculo;
 }
