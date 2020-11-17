5f5ae1eb3857f69fc3717992b6c3b3e870b201b8
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-17 11:04:45 -0300

    Update module-info.java

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -1,2 +1,10 @@
 module app.calculo {
+	
+	/*
+	 * Lista de pacotes que serão exportados (podem ser usados fora do projeto)
+	 * Os pacotes internos não são exportados por padrão, todos os pacotes
+	 * devem ser descritos aqui para que sejam visíveis para outros projetos.
+	 */
+	
+	exports io.github.heltonricardo.app.calculo;
 }
