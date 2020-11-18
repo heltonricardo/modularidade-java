d5e17503d41d5489131656a4ffb539879c80614e
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-18 11:48:57 -0300

    Update module-info.java

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -1,3 +1,12 @@
+/* Usando:
+ * open module app.calculo {
+ * Esse pacote será aberto para todos os que o importarem, tornando possível
+ * alterar classes e objetos via metadados (consulte Teste.java de
+ * app-financeiro). Também é possível abrí-lo somente para um projeto
+ * específico, usando o opens, que aceita mais de um nome de projeto separado
+ * por vírgulas.
+ */
+
 module app.calculo {
 	
 	/*
@@ -8,8 +17,18 @@ module app.calculo {
 	 * Usamos o transitive quando queremos que quem importar esse projeto
 	 * também importe o transitivo, pois há uma dependência de códigos que
 	 * estão no projeto que esse projeto importou.
+	 * 
+	 * Exports ... to ... exporta pacotes somente para projetos específicos.
+	 * É possível separar os nomes dos projetos por vírgula.
 	 */
 	
 	requires transitive app.login;
 	exports io.github.heltonricardo.app.calculo;
+	
+	exports io.github.heltonricardo.app.calculo.interno
+		 to app.financeiro;
+	
+	// opens io.github.heltonricardo.app.calculo; para todos ou:
+	opens io.github.heltonricardo.app.calculo
+	   to app.financeiro;
 }
