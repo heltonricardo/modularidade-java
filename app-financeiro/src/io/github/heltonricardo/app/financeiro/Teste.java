817ce6e00e2dd89504cbd49fc9947136cb06c3ae
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-18 15:21:44 -0300

    Update Teste.java

--- app-financeiro/src/io/github/heltonricardo/app/financeiro/Teste.java
+++ app-financeiro/src/io/github/heltonricardo/app/financeiro/Teste.java
@@ -1,9 +1,9 @@
 package io.github.heltonricardo.app.financeiro;
 
 import java.lang.reflect.Field;
+import java.util.ServiceLoader;
 
-import io.github.heltonricardo.app.calculo.Calculadora;
-import io.github.heltonricardo.app.calculo.interno.OperacoesAritmeticas;
+import io.github.heltonricardo.app.Calculadora;
 
 /*
  * Em Java é possível alterar dados privados de classes e objetos! Basta
@@ -11,27 +11,32 @@ import io.github.heltonricardo.app.calculo.interno.OperacoesAritmeticas;
  * Porém só é possível fazê-lo quando o pacote importado que contém a classe
  * que quero alterar está "aberto" no module-info do projeto que o exporta.
  * Nesse caso, verifique o arquivo do projeto app-calculo.
+ * 
+ * Possuímos a importação de uma interface, e usamos o ServiceLoader para
+ * procurar, em todos os projetos que estão na Build Path, todas as classes que
+ * implementam a interface. Ele pode voltar nenhuma ou muitas. Nesse caso
+ * consideramos que ele encontra mais de zero implementações e pegamos a
+ * primeira delas. Devemos usar o .class nos objetos pois não é possível saber
+ * o nome da classe que implementou a interface.
  */
 
 public class Teste {
 
 	public static void main(String[] args) {
 		
-		Calculadora calc = new Calculadora();
+		Calculadora calc = ServiceLoader
+				.load(Calculadora.class)
+				.findFirst()
+				.get();
 		
 		System.out.println(calc.soma(1.1, 2.2, 3.3, 4.4, 5.5));
-		
-		System.out.println(calc.getLoggerClass());
-		
-		OperacoesAritmeticas op = new OperacoesAritmeticas();
-		System.out.println(op.soma(6, 7, 8, 9));
-		
+	
 		calc.setId("meu_id");
 		System.out.println(calc.getId());
 		
 		try {
 			// fildId "aponta" para o id da classe Calculadora:			
-			Field fildId = Calculadora.class.getDeclaredFields()[0];
+			Field fildId = calc.getClass().getDeclaredFields()[0];
 			
 			// Torna o atributo passível de alteração / visualização:
 			fildId.setAccessible(true);
