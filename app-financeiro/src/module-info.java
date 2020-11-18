dc0db062121e714a1ca9e656ed6b65fe2e348651
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-18 15:21:47 -0300

    Update module-info.java

--- app-financeiro/src/module-info.java
+++ app-financeiro/src/module-info.java
@@ -8,8 +8,20 @@ module app.financeiro {
 	 * Projects > Selecione modulepath > Add > Selecione o projeto para
 	 * importar > Ok > Apply and Close. Em seguida já será possível usar o
 	 * requires aqui dos pacotes exportados pelo projeto adicionado.
+	 * 
+	 * Não precisamos importar app.calculo aqui pois já importamos app.api, que
+	 * é o que possui a interface que precisamos. Mesmo assim precisamos manter
+	 * em Build Path o projeto app-calculo, pois nele possui uma implementação
+	 * para a interface usada.
+	 * 
+	 * O "uses" declara qual serviço será usado dentro da aplicação. Então além
+	 * de importar o pacote de projeto, deve-se informar que o serviço
+	 * Calculadora será usado. É chamado de serviço pois é apenas trata-se
+	 * apenas de uma interface, e não da própria implementação, como já dito.
 	 */
 	
-	requires app.calculo;
 	requires app.login;
+	
+	requires app.api;
+	uses io.github.heltonricardo.app.Calculadora;
 }
